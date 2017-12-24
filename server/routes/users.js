ee9748125f175fa574864c8d188da7aa5e2a322f
unknown <orhan.hajderovic@hotmail.com>
2017-12-24 19:31:36 +0100

    register user

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,84 +7,77 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+// Authenticate the user
+router.post('/login', (req, res, next) => {
+  res.send('works');
+});
+
 // Add user to DB
-router.post('/register', function (req, res, next) {
+router.post('/register', (req, res, next) => {
 
-  let promise = new Promise((resolve, reject) => {
-
-    // Check if user has provided information
-    if (!req.body.firstName) {
-      reject();
+  // Make sure user no fields are left empty
+  if (!req.body.firstName) {
+    res.json({ success: false, message: 'Please enter a firstname' })
+  } else {
+    if (!req.body.lastName) {
+      res.json({ success: false, message: 'Please enter a lastname' })
     } else {
-      if (!req.body.lastName) {
-        reject();
+      if (!req.body.userName) {
+        res.json({ success: false, message: 'Please enter a username' })
       } else {
-        if (!req.body.userName) {
-          reject();
+        if (!req.body.email) {
+          res.json({ success: false, message: 'Please enter an email' })
         } else {
-          if (!req.body.email) {
-            reject();
+          if (!req.body.password) {
+            res.json({ success: false, message: 'Please enter a password' })
           } else {
-            if (!req.body.password) {
-              reject();
-            } else {
-
-              // Call resolve
-              resolve();
-            }
+            createUser();
           }
         }
       }
     }
-  });
+  }
 
-  // Catch promise error => throw
-  promise.catch(() => {
-    res.json({ success: false, message: 'Some fields are missing, please enter information on all required fields' });
-  })
+  function createUser() {
+    // Create new instance of the user Object
+    let user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password
+    });
 
-    // In case user enters all credentials => resolve promise
-    .then(() => {
-      // Create new instance of the user Object
-      let user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        userName: req.body.userName,
-        email: req.body.email,
-        password: req.body.password
-      });
-
-      // Save user to DB
-      user.save()
-        // Promise resolved => send success message
-        .then(() => {
-          res.json({ success: true, message: 'Logged in' });
-        })
-        // Promise rejected => Throw error
-        .catch((err) => {
-          if (err) {
-            res.json({ success: false, error: err.message })
-            if (err.errors) {
-              if (error.errors.firstName) {
-                res.json({ success: false, error: 'firstname already exists' });
+    // Save user to DB
+    user.save()
+      // Promise resolved => send success message
+      .then(() => {
+        res.json({ success: true, message: 'Logged in' });
+      })
+      // Promise rejected => Throw error
+      .catch((err) => {
+        if (err) {
+          res.json({ success: false, error: err.message })
+          if (err.errors) {
+            if (error.errors.firstName) {
+              res.json({ success: false, error: 'firstname already exists' });
+            } else {
+              if (error.errors.lastName) {
+                res.json({ success: false, error: 'lastname already exists' });
               } else {
-                if (error.errors.lastName) {
-                  res.json({ success: false, error: 'lastname already exists' });
+                if (error.errors.userName) {
+                  res.json({ success: false, error: 'Username already exists' });
                 } else {
-                  if (error.errors.userName) {
-                    res.json({ success: false, error: 'Username already exists' });
-                  } else {
-                    if (error.errors.email) {
-                      res.json({ success: false, error: 'Eamail already exists' });
-                    }
+                  if (error.errors.email) {
+                    res.json({ success: false, error: 'Eamail already exists' });
                   }
                 }
               }
             }
           }
-        })
-    })
-
+        }
+      });
+  }
 });
 
 
